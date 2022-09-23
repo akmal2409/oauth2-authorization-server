b125eee0ceb1c52bd6d4a22ee38efdc5abeccc3f
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2022-09-23 14:30:46 +0200

    Add ByteUtilsTest.java to test longToByteArray()

---
+++ src/test/java/com/akmal/oauth2authorizationserver/utils/ByteUtilsTest.java
@@ -0,0 +1,23 @@
+package com.akmal.oauth2authorizationserver.utils;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class ByteUtilsTest {
+
+  @Test
+  void longToByteArray() {
+    long givenNumber = 1152902738463079355L;
+    byte[] expected = new byte[]{(byte) 0x0F, (byte) 0xFF, (byte) 0xEE, (byte) 0xEE, (byte) 0xAA,
+        (byte) 0xAA, (byte) 0xBB, (byte) 0xBB};
+
+    // when
+    byte[] actual = ByteUtils.longToByteArray(givenNumber);
+
+    //then
+    assertThat(actual)
+        .isNotNull()
+        .containsExactly(expected);
+  }
+}
