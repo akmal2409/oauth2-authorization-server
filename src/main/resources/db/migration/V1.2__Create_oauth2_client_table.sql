e449db1235f892a904d865f79054a20045ff04e6
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2022-09-23 14:31:54 +0200

    Update V1.2__Create_oauth2_client_table.sql migration to include allow wildcards field

--- src/main/resources/db/migration/V1.2__Create_oauth2_client_table.sql
+++ src/main/resources/db/migration/V1.2__Create_oauth2_client_table.sql
@@ -2,5 +2,6 @@ CREATE TABLE Clients (
     client_id VARCHAR(36) PRIMARY KEY,
     client_secret VARCHAR(36) NOT NULL,
     name VARCHAR(50) NOT NULL,
-    require_user_consent BOOLEAN DEFAULT TRUE
+    require_user_consent BOOLEAN DEFAULT TRUE,
+    allow_wildcards_in_redirect_urls BOOLEAN DEFAULT FALSE
 );
