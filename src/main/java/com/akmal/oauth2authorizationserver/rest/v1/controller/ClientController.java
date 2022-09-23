aa23357a9dae6e5ef7f87775dc0eccc74f39ce81
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2022-09-23 14:36:52 +0200

    Create ClientController.java a rest controller.

---
+++ src/main/java/com/akmal/oauth2authorizationserver/rest/v1/controller/ClientController.java
@@ -0,0 +1,40 @@
+package com.akmal.oauth2authorizationserver.rest.v1.controller;
+
+import com.akmal.oauth2authorizationserver.rest.v1.dto.client.ClientDto;
+import com.akmal.oauth2authorizationserver.rest.v1.dto.client.SecretGenerationResponse;
+import com.akmal.oauth2authorizationserver.rest.v1.dto.client.action.ClientCreateAction;
+import com.akmal.oauth2authorizationserver.service.v1.ClientService;
+import java.util.List;
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(ClientController.BASE_API_URL)
+@RequiredArgsConstructor
+public class ClientController {
+  public static final String BASE_API_URL = "/api/v1/clients";
+  private final ClientService clientService;
+
+  @GetMapping
+  public List<ClientDto> findAllClients() {
+    return this.clientService.findAllClients();
+  }
+
+  @PostMapping
+  public ClientDto createClient(@RequestBody @Valid ClientCreateAction clientCreateAction) {
+    return this.clientService.create(clientCreateAction);
+  }
+
+  @PutMapping("/{clientId}/secrets")
+  public SecretGenerationResponse generateSecret(@PathVariable @Valid @NotEmpty String clientId) {
+    return this.clientService.generateSecret(clientId);
+  }
+}
