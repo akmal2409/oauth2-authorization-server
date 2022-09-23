7b28137c259c95233c1cf2861030c6c5c2ff7e42
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2022-09-23 15:20:10 +0200

    Drop NOT NULL constraint from Client migration file.
    Dropped constraint on client_secret column because the secret is generated on demand (for enhanced security).

--- src/main/resources/db/migration/V1.2__Create_oauth2_client_table.sql
+++ src/main/resources/db/migration/V1.2__Create_oauth2_client_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Clients (
     client_id VARCHAR(36) PRIMARY KEY,
-    client_secret VARCHAR(36) NOT NULL,
+    client_secret VARCHAR(36),
     name VARCHAR(50) NOT NULL,
     require_user_consent BOOLEAN DEFAULT TRUE,
     allow_wildcards_in_redirect_urls BOOLEAN DEFAULT FALSE
