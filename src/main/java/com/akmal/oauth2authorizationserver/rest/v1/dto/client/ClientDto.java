d443b0027f293f40f85c53d935b1ce47e41983e0
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2022-09-23 14:34:38 +0200

    Create ClientCreateAction.java, ClientDto.java and ClientUpdateAction.java dto objects

---
+++ src/main/java/com/akmal/oauth2authorizationserver/rest/v1/dto/client/ClientDto.java
@@ -0,0 +1,31 @@
+package com.akmal.oauth2authorizationserver.rest.v1.dto.client;
+
+import com.akmal.oauth2authorizationserver.model.client.Client;
+import java.util.List;
+import java.util.Optional;
+
+public record ClientDto(
+    String clientId,
+    String name,
+    List<String> grants,
+    List<String> signInRedirectUris,
+    List<String> signOutRedirectUris,
+    List<String> trustedOrigins,
+    boolean requireUserConsent
+) {
+
+  public static ClientDto from(Client client) {
+    return new ClientDto(
+        client.getClientId(),
+        client.getName(),
+        Optional.ofNullable(client.getGrants())
+            .map(gs -> gs.stream().map(g -> g.getType().toString())
+                           .toList())
+            .orElse(List.of()),
+        Optional.ofNullable(client.getSignInRedirectUris()).orElse(List.of()),
+        Optional.ofNullable(client.getSignOutRedirectUris()).orElse(List.of()),
+        Optional.ofNullable(client.getTrustedOrigins()).orElse(List.of()),
+        client.isRequireUserConsent()
+    );
+  }
+}
---
+++ src/main/java/com/akmal/oauth2authorizationserver/rest/v1/dto/client/action/ClientUpdateAction.java
@@ -0,0 +1,33 @@
+package com.akmal.oauth2authorizationserver.rest.v1.dto.client.action;
+
+import com.akmal.oauth2authorizationserver.model.client.Client;
+import com.akmal.oauth2authorizationserver.model.client.Grant;
+import com.akmal.oauth2authorizationserver.model.client.GrantType;
+import java.util.List;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+public record ClientUpdateAction(
+    @NotNull String name,
+    @NotNull @NotEmpty List<String> grants,
+    @NotNull List<String> signInRedirectUris,
+    @NotNull List<String> signOutRedirectUris,
+    @NotNull List<String> trustedOrigins,
+    boolean requireUserConsent,
+    boolean allowWildcardsInRedirectUrls
+) {
+
+  public Client toClient() {
+    return new Client(
+        null,
+        this.name,
+        null,
+        this.grants.stream().map(GrantType::valueOf).map(Grant::new).toList(),
+        this.signInRedirectUris,
+        this.signOutRedirectUris,
+        this.trustedOrigins,
+        this.requireUserConsent,
+        this.allowWildcardsInRedirectUrls
+    );
+  }
+}
