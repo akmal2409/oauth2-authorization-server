162ce6af2a53abd700616477f08ab66bb9314cc8
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2022-09-23 14:32:30 +0200

    Add spring-security-starter to pom.xml and exclude it from autoconfiguration

--- src/main/java/com/akmal/oauth2authorizationserver/Oauth2AuthorizationServerApplication.java
+++ src/main/java/com/akmal/oauth2authorizationserver/Oauth2AuthorizationServerApplication.java
@@ -1,9 +1,11 @@
 package com.akmal.oauth2authorizationserver;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
 public class Oauth2AuthorizationServerApplication {
 
   public static void main(String[] args) {
