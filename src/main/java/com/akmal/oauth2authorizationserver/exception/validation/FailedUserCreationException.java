f427a65c2946eab6058e1b08b9a54dbf1eb2812b
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2022-09-25 20:39:58 +0200

    Create two custom exceptions FailedUserCreationException and InternalServerErrorException

---
+++ src/main/java/com/akmal/oauth2authorizationserver/exception/validation/FailedUserCreationException.java
@@ -0,0 +1,8 @@
+package com.akmal.oauth2authorizationserver.exception.validation;
+
+public class FailedUserCreationException extends AbstractValidationException {
+
+  public FailedUserCreationException(String message) {
+    super(message);
+  }
+}
