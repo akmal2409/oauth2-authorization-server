97005ed238184b0baeda73c268caa61ef794e0e6
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2022-09-27 20:15:16 +0200

    Add SessionCookieAuthentication implementation of the Authentication interface.
    Added representation of the authenticated principal that is set upon successful login.

---
+++ src/main/java/com/akmal/oauth2authorizationserver/internal/authentication/SessionCookieAuthentication.java
@@ -0,0 +1,61 @@
+package com.akmal.oauth2authorizationserver.internal.authentication;
+
+import java.security.Principal;
+import java.util.Collection;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+public class SessionCookieAuthentication implements Authentication {
+  private final Collection<? extends GrantedAuthority> authorities;
+  private final String sessionId;
+  private final String sub;
+  private boolean authenticated;
+
+  public SessionCookieAuthentication(Collection<? extends GrantedAuthority> authorities,
+      String sessionId, String sub, boolean authenticated) {
+    this.authorities = authorities;
+    this.sessionId = sessionId;
+    this.sub = sub;
+    this.authenticated = authenticated;
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return this.authorities;
+  }
+
+  @Override
+  public Object getCredentials() {
+    return this.sessionId;
+  }
+
+  @Override
+  public Object getDetails() {
+    return null;
+  }
+
+  @Override
+  public Object getPrincipal() {
+    return new Principal() {
+      @Override
+      public String getName() {
+        return sub;
+      }
+    };
+  }
+
+  @Override
+  public boolean isAuthenticated() {
+    return this.authenticated;
+  }
+
+  @Override
+  public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+    this.authenticated = isAuthenticated;
+  }
+
+  @Override
+  public String getName() {
+    return this.sub;
+  }
+}
