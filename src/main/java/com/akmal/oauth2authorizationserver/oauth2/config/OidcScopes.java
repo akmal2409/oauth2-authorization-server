3e29541e897a70b95a041bda88f1c9c8f27a0ccf
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2022-09-28 22:10:53 +0200

    Create OidcScopes set of constants and updated OIDC scopes migrations

---
+++ src/main/java/com/akmal/oauth2authorizationserver/oauth2/config/OidcScopes.java
@@ -0,0 +1,13 @@
+package com.akmal.oauth2authorizationserver.oauth2.config;
+
+public class OidcScopes {
+
+  private OidcScopes() {}
+
+  public static final String OPENID = "openid";
+  public static final String PROFILE = "profile";
+  public static final String EMAIL = "email";
+  public static final String ADDRESS = "address";
+  public static final String PHONE = "phone";
+  public static final String OFFLINE_ACCESS = "offline_access";
+}
--- src/main/resources/db/migration/V1.8__Create_Scopes_Table_With_OIDC_Scopes.sql
+++ src/main/resources/db/migration/V1.8__Create_Scopes_Table_With_OIDC_Scopes.sql
@@ -10,4 +10,7 @@ CREATE INDEX scope_name_idx ON Scopes(name);
 INSERT INTO Scopes VALUES
 (1, 'openid', true, 'Allow application to verify your identity'),
 (2, 'profile', true, 'Allow application to access to basic information such as name, username, picture etc'),
+(3, 'address', true, 'Allow application to view your address'),
+(4, 'phone', true, 'Allow application to view your phone'),
+(5, 'offline_access', true, 'Allow application to renew access to your account automatically'),
 (3, 'email', true, 'Allow application to see your email address');
