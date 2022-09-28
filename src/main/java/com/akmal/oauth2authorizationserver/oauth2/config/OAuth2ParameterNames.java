adfea90e7268adf4bc35cfdcae104627b3110ae5
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2022-09-28 22:07:32 +0200

    Add OAuth2ParameterNames set of names of the parameters defined by the OAuth2 specification.

---
+++ src/main/java/com/akmal/oauth2authorizationserver/oauth2/config/OAuth2ParameterNames.java
@@ -0,0 +1,37 @@
+package com.akmal.oauth2authorizationserver.oauth2.config;
+
+/**
+ * Defines constants (mappings) for authentication attributes to assist extraction and serialization.
+ */
+public class OAuth2ParameterNames {
+
+  public static final String CLIENT_ID = "client_id";
+  public static final String ACCESS_TOKEN = "access_token";
+  public static final String ASSERTION = "assertion";
+  public static final String CLIENT_ASSERTION = "client_assertion";
+  public static final String CLIENT_ASSERTION_TYPE = "client_assertion_type";
+  public static final String CLIENT_SECRET = "client_secret";
+  public static final String CODE = "code";
+  public static final String ERROR = "error";
+  public static final String ERROR_DESCRIPTION = "error_description";
+  public static final String ERROR_URI = "error_uri";
+  public static final String EXPIRES_IN = "expires_in";
+  public static final String GRANT_TYPE = "grant_type";
+  public static final String PASSWORD = "password";
+  public static final String REFRESH_TOKEN = "refresh_token";
+  public static final String TOKEN_TYPE = "token_type";
+  public static final String TOKEN_TYPE_HINT = "token_type_hint";
+  public static final String USERNAME = "username";
+
+  public static final String REDIRECT_URI = "redirect_uri";
+  public static final String RESPONSE_TYPE = "response_type";
+  public static final String STATE = "state";
+  public static final String CODE_CHALLENGE = "code_challenge";
+  public static final String CODE_CHALLENGE_METHOD = "code_challenge_method";
+  public static final String NONCE = "nonce";
+  public static final String IDP = "idp";
+  public static final String IDP_SCOPE = "idp_scope";
+  public static final String SCOPE = "scopes";
+
+  private OAuth2ParameterNames() {}
+}
