8cb53ac70d6c6551bb69a1c1e52497210720fff2
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2022-10-02 17:02:12 +0200

    Add set of session related constants in AuthenticationHttpSessionAttributes

---
+++ src/main/java/com/akmal/oauth2authorizationserver/oauth2/AuthenticationHttpSessionAttributes.java
@@ -0,0 +1,10 @@
+package com.akmal.oauth2authorizationserver.oauth2;
+
+public class AuthenticationHttpSessionAttributes {
+
+  /**
+   * The URL to which we need to redirect after the sign in.
+   */
+  public static final String TARGET_URL = "target_url";
+  public static final String SSO_SESSION_ID = "sso_session_id";
+}
