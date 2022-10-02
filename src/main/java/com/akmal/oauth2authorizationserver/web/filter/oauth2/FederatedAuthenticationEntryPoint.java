2cabc9cadf5bd24166a0de3ef228b5251028c2b2
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2022-10-02 17:03:05 +0200

    Create custom FederatedAuthenticationEntryPoint that will route based on the presence of the IDP request paramter.

---
+++ src/main/java/com/akmal/oauth2authorizationserver/web/filter/oauth2/FederatedAuthenticationEntryPoint.java
@@ -0,0 +1,47 @@
+package com.akmal.oauth2authorizationserver.web.filter.oauth2;
+
+import com.akmal.oauth2authorizationserver.oauth2.AuthenticationHttpSessionAttributes;
+import com.akmal.oauth2authorizationserver.oauth2.config.OAuth2ParameterNames;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.util.StringUtils;
+
+/**
+ * The class serves a purpose as the last component in the filter chain when the authentication fails
+ * and is invoked just before sending the exception to the user. It is used to redirect the user
+ * to the login page or in case if the idp query paramter is present, it will redirect to the respective
+ * 3rd party Idp.
+ */
+@Slf4j
+public class FederatedAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+  private final AuthenticationEntryPoint webBasedLoginEntryPoint;
+  private final RedirectStrategy redirectStrategy;
+
+  public FederatedAuthenticationEntryPoint(AuthenticationEntryPoint webBasedLoginEntryPoint,
+      RedirectStrategy redirectStrategy) {
+    this.webBasedLoginEntryPoint = webBasedLoginEntryPoint;
+    this.redirectStrategy = redirectStrategy;
+  }
+
+  @Override
+  public void commence(HttpServletRequest request, HttpServletResponse response,
+      AuthenticationException authException) throws IOException, ServletException {
+    final var idp = request.getParameter(OAuth2ParameterNames.IDP);
+
+    if (StringUtils.hasText(idp)) {
+      // federate the authentication to the 3rd party Idp after validation
+    } else {
+      // redirect user to the login page with the targetUrl query parameter set to the request url
+      final var targetUrl = request.getRequestURL().toString();
+      request.getSession().setAttribute(AuthenticationHttpSessionAttributes.TARGET_URL, targetUrl);
+      this.webBasedLoginEntryPoint.commence(request, response, authException);
+    }
+  }
+}
