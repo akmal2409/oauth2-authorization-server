40a92d2ad28dbb9cb2c59034f65c76c5daa250ca
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2022-10-12 19:56:29 +0200

    Make code challenge mandatory during authorization request

--- src/main/java/com/akmal/oauth2authorizationserver/oauth2/authconverter/OAuth2WebFlowRequestAuthenticationConverter.java
+++ src/main/java/com/akmal/oauth2authorizationserver/oauth2/authconverter/OAuth2WebFlowRequestAuthenticationConverter.java
@@ -1,7 +1,5 @@
 package com.akmal.oauth2authorizationserver.oauth2.authconverter;
 
-import com.akmal.oauth2authorizationserver.exception.oauth2.OAuth2AuthorizationException;
-import com.akmal.oauth2authorizationserver.oauth2.OAuth2Error;
 import com.akmal.oauth2authorizationserver.oauth2.authentication.OAuth2WebFlowAuthenticationDetails;
 import com.akmal.oauth2authorizationserver.oauth2.authentication.OAuth2WebFlowRequestAuthentication;
 import com.akmal.oauth2authorizationserver.oauth2.config.OAuth2ErrorTypes;
@@ -14,16 +12,14 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import javax.servlet.http.HttpServletRequest;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationDetailsSource;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.AuthenticationConverter;
 import org.springframework.util.StringUtils;
 
-public class OAuth2WebFlowRequestAuthenticationConverter implements AuthenticationConverter  {
+public class OAuth2WebFlowRequestAuthenticationConverter extends
+    AbstractOAuth2AuthenticationConverter {
   public static final String ERROR_URI_HTTP_REQUEST_SPECS = "https://datatracker.ietf.org/doc/html/rfc6749#section-4.2.1";
   private final AuthenticationDetailsSource<HttpServletRequest, OAuth2WebFlowAuthenticationDetails> detailsSource;
 
@@ -63,8 +59,7 @@ public class OAuth2WebFlowRequestAuthenticationConverter implements Authenticati
     }
 
     // minimum 1 scope is required for authentication such as openid
-    final var scopes = StringUtils.hasText(request.getParameter(OAuth2ParameterNames.SCOPE)) ?
-                           Arrays.asList(request.getParameter(OAuth2ParameterNames.SCOPE).split(",")) : List.<String>of();
+    final var scopes = extractMultiValueParam(request, OAuth2ParameterNames.SCOPE, ",");
     if (!scopes.contains(OidcScopes.OPENID)) {
       throwError(OAuth2ErrorTypes.INVALID_REQUEST, "openid scope is required", null,
           request.getParameter(OAuth2ParameterNames.STATE));
@@ -73,17 +68,17 @@ public class OAuth2WebFlowRequestAuthenticationConverter implements Authenticati
     // other optional parameters
     final var state = request.getParameter(OAuth2ParameterNames.STATE);
     final var codeChallenge = request.getParameter(OAuth2ParameterNames.CODE_CHALLENGE);
+    if (!StringUtils.hasText(codeChallenge)) throwError(OAuth2ErrorTypes.INVALID_REQUEST, "code_challenge is missing", ERROR_URI_HTTP_REQUEST_SPECS, state);
+
     var codeChallengeMethod = OAuth2CodeChallengeMethod.from(request.getParameter(OAuth2ParameterNames.CODE_CHALLENGE_METHOD));
 
-    if (StringUtils.hasText(codeChallenge) && codeChallengeMethod == null) {
+    if (codeChallengeMethod == null) {
       codeChallengeMethod = OAuth2CodeChallengeMethod.S256;
     }
 
     final var nonce = request.getParameter(OAuth2ParameterNames.NONCE);
     final var idp = request.getParameter(OAuth2ParameterNames.IDP);
-    final var idpScopes = StringUtils.hasText(request.getParameter(OAuth2ParameterNames.IDP_SCOPE)) ?
-                              Arrays.asList(request.getParameter(OAuth2ParameterNames.IDP_SCOPE).split(",")) :
-                              List.<String>of();
+    final var idpScopes = extractMultiValueParam(request, OAuth2ParameterNames.IDP_SCOPE, ",");
     final var responseMode = OAuth2ResponseMode.from(request.getParameter(OAuth2ParameterNames.RESPONSE_MODE));
 
     final var internalAuthentication = SecurityContextHolder.getContext().getAuthentication(); // nullable, for success auth request needs to be set by the preceding filter-chain
@@ -107,20 +102,5 @@ public class OAuth2WebFlowRequestAuthenticationConverter implements Authenticati
     );
   }
 
-  /**
-   * Helper method that throws OAuth2 compliant error wrapped in a {@link OAuth2AuthorizationException}
-   * class that must be intercepted globally and returned in accordance with the
-   * {@link OAuth2Error} schema.
-   *
-   * @param error one of the {@link com.akmal.oauth2authorizationserver.oauth2.config.OAuth2ErrorTypes}
-   * @param description human readable description (optional)
-   * @param errorUri to the official specs (optional)
-   * @param state that was sent by the user (optional)
-   */
-  private static void throwError(@NotNull String error, @Nullable String description,
-      @Nullable String errorUri, @Nullable String state) {
-    throw new OAuth2AuthorizationException(
-        new OAuth2Error(error, description, errorUri, state)
-    );
-  }
+
 }
