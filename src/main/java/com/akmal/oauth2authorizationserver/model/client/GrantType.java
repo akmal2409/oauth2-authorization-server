0e9d657eb5879c28b1f13c46192e74ccd875b0d1
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2022-10-12 19:51:05 +0200

    Add static factory to GrantType.java to get enum by name

--- src/main/java/com/akmal/oauth2authorizationserver/model/client/GrantType.java
+++ src/main/java/com/akmal/oauth2authorizationserver/model/client/GrantType.java
@@ -1,5 +1,16 @@
 package com.akmal.oauth2authorizationserver.model.client;
 
+import org.springframework.util.StringUtils;
+
 public enum GrantType {
-  AUTHORIZATION_CODE_PKCE, REFRESH_TOKEN, CLIENT_CREDENTIALS, HYBRID, DEVICE_CODE
+  AUTHORIZATION_CODE_PKCE, REFRESH_TOKEN, CLIENT_CREDENTIALS, HYBRID, DEVICE_CODE;
+
+  public static GrantType from(String name) {
+    if ("authorization_code".equalsIgnoreCase(name)) return AUTHORIZATION_CODE_PKCE;
+    for (GrantType grant: values()) {
+      if (grant.name().equalsIgnoreCase(name)) return grant;
+    }
+
+    return null;
+  }
 }
