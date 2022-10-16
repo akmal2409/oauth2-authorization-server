06daaf5542cc7ab81b596c9e5cceadd8dc5f1628
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2022-10-16 16:28:41 +0200

    Enable async task execution in the app with @EnableAsync

--- src/main/java/com/akmal/oauth2authorizationserver/Oauth2AuthorizationServerApplication.java
+++ src/main/java/com/akmal/oauth2authorizationserver/Oauth2AuthorizationServerApplication.java
@@ -1,11 +1,13 @@
 package com.akmal.oauth2authorizationserver;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
+@SpringBootApplication
+@EnableAsync
 public class Oauth2AuthorizationServerApplication {
 
   public static void main(String[] args) {
